f69a27876d2199b97d9924757e12973786002fb7
Cézar Santos <cezar.augusto@venturus.org.br>
2026-05-20 18:15:05 -0300

    Second commit

---
+++ src/main/java/com/example/ci_training/CiTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ci_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ci_training/CiTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ci_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
